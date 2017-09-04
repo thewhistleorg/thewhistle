e96b15664d5faa573f117d5e94b1a994a1ee9ce4
Chris Veness <chrisv@movable-type.co.uk>
2017-09-04 08:47:53 +0100

    Move (organisation-specific) db connection to project-specific subapp
    The name of the database connection to use is taken from the URL; this is not
    possible from within app-report.js, so it is moved down to <database>/<project>/
    app.js (previously the TEST database was hard-wired).

--- app-report/test/sexual-assault/app.js
+++ app-report/test/sexual-assault/app.js
@@ -1,14 +1,36 @@
 /* - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - -  */
-/* App: test/sexual-assault                                                                       */
+/* App: civilian (public) incident reporting (boilerplate).                                       */
+/*                                                                                                */
+/* This is a composed sup-app, in order that the database and project for the MongoDB connection  */
+/* and the handlebars templates can be taken from the URL.                                        */
+/*                                                                                                */
+/* This app.js is identical for all databases/projects - if we can work out a mechanism to invoke */
+/* the routes directly from app-report.js this could be factored back there.                      */
 /* - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - -  */
 
 'use strict';
 
-const Koa        = require('koa');            // koa framework
-const handlebars = require('koa-handlebars'); // handlebars templating
+const Koa         = require('koa');                 // koa framework
+const handlebars  = require('koa-handlebars');      // handlebars templating
+const MongoClient = require('mongodb').MongoClient; // official MongoDB driver for Node.js
 
 
 const app = new Koa(); // report app
+
+
+// get database connection if not already available
+app.use(async function getDbConnection(ctx, next) {
+    if (!global.db[ctx.params.database]) {
+        try {
+            const connectionString = process.env['DB_'+ctx.params.database.toUpperCase()];
+            global.db[ctx.params] = await MongoClient.connect(connectionString);
+        } catch (e) {
+            app.throw(e);
+        }
+    }
+    await next();
+});
+
 
 const hbsCheckedHelper = function(value, options) {
     const div = document.createElement('div'); // create a container div
@@ -37,6 +59,7 @@ app.use(handlebars({
     partialsDir:   './templates/partials',
     helpers:       { checked: hbsCheckedHelper },
 }));
+
 
 app.use(require('./routes.js'));
 

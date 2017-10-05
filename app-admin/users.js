a20f94b3a3918c9421274238c51fe0f350403749
Chris Veness <chrisv@movable-type.co.uk>
2017-10-05 17:13:34 +0100

    Add su role, restrict org'ns to su users

--- app-admin/users.js
+++ app-admin/users.js
@@ -11,41 +11,86 @@
 
 const User   = require('../models/user.js');
 
+/*
+ * Note on roles:
+ *  - reporter: for paralegals who can log in to use the admin single-page reporting function, but
+ *              have access to no other parts of the admin system
+ *  - user:     regular NGO admin system user
+ *  - admin:    NGO administrator user with rights to manage users and resources
+ *  - su:       user who can view & manage users from all organisations
+ */
+
 class UsersHandlers {
 
     /**
      * GET /users/add - Render add user page.
      */
     static async add(ctx) {
-        const availableDatabases = Object.keys(global.db).filter(db => db!='users');
-        const context = Object.assign({ availableDatabases }, ctx.flash.formdata);
-        await ctx.render('users-add', context);
+        const isAdminUser = ctx.state.user.roles.includes('admin');
+        if (!isAdminUser) {
+            ctx.flash = { _error: 'User management requires admin privileges' };
+            ctx.redirect('/login');
+        }
+
+        const isSuUser = ctx.state.user.roles.includes('su') ? 'show' : 'hide';
+        const context = {
+            isSuUser:           isSuUser,             // su can view users from all organisations, and set org access
+            availableDatabases: availableDatabases(), // su can set which organisations use can access
+            databases:          ctx.state.user.db,    // make current user's organisation checked by default
+        };
+
+        await ctx.render('users-add', Object.assign(context, ctx.flash.formdata));
     }
+
 
     /**
      * GET /users - Render list users page.
+     *
+     * If user has su role, all users are listed; otherwise, only users with access to current
+     * user's database.
      */
     static async list(ctx) {
-        const users = await User.getAll();
+        const isAdminUser = ctx.state.user.roles.includes('admin');
+        if (!isAdminUser) {
+            ctx.flash = { _error: 'User management requires admin privileges' };
+            ctx.redirect('/login');
+        }
+
+        const isSuUser = ctx.state.user.roles.includes('su') ? 'show' : 'hide';
+
+        const users = (await User.getAll()).filter(usr => isSuUser=='show' || usr.databases.includes(ctx.state.user.db));
+
         users.sort((a, b) => { a = (a.firstname+a.lastname).toLowerCase(); b = (b.firstname+b.lastname).toLowerCase(); return a < b ? -1 : 1; });
-        await ctx.render('users-list', { users });
+        await ctx.render('users-list', { users, isSuUser: isSuUser });
     }
+
 
     /**
      * GET /users/edit - Render edit user page.
      */
     static async edit(ctx) {
+        const isAdminUser = ctx.state.user.roles.includes('admin');
+        if (!isAdminUser) {
+            ctx.flash = { _error: 'User management requires admin privileges' };
+            ctx.redirect('/login');
+        }
+
         const user = await User.get(ctx.params.id);
 
-        const availableDatabases = Object.keys(global.db).filter(db => db!='users');
+        const isSuUser = ctx.state.user.roles.includes('su') ? 'show' : 'hide';
+        const context = {
+            isSuUser:           isSuUser,             // su can view users from all organisations, and set org access
+            availableDatabases: availableDatabases(), // su can set which organisations use can access
+        };
 
-        const context = Object.assign(user, { availableDatabases }, ctx.flash.formdata);
-        await ctx.render('users-edit', context);
+        await ctx.render('users-edit', Object.assign(user, context, ctx.flash.formdata));
     }
+
 
     /* - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - -  */
     /* POST processing                                                                            */
     /* - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - -  */
+
 
     /**
      * POST /users/add - Process add user.
@@ -55,7 +100,7 @@ class UsersHandlers {
 
         try {
 
-            // username must be
+            // username must be alphanumeric
             if (!ctx.request.body.username.match(/[a-zA-Z-][a-zA-Z0-9-]*/)) throw new Error('Username must be alphanumeric');
 
             // ensure roles is array (koa-body will return single selection as string not array)
@@ -65,6 +110,11 @@ class UsersHandlers {
             // ensure databases is array (koa-body will return single selection as string not array)
             if (!Array.isArray(ctx.request.body.databases)) {
                 ctx.request.body.databases = ctx.request.body.databases ? [ ctx.request.body.databases ] : [];
+            }
+
+            // ensure su is also admin
+            if (ctx.request.body.roles.includes('su') && !ctx.request.body.roles.includes('admin')) {
+                ctx.request.body.roles.concat(['admin'], ctx.request.body.roles); // put 'admin' at the front!
             }
 
             const id = await User.insert(ctx.request.body);
@@ -85,6 +135,9 @@ class UsersHandlers {
 
     /**
      * POST /users/:username/edit - Process edit user.
+     *
+     * TODO: server-side validation - only admin user should be able to change admin status, but
+     *       needs thought as to implementation
      */
     static async processEdit(ctx) {
         if (!ctx.state.user.roles.includes('admin')) return ctx.redirect('/login'+ctx.url);
@@ -103,7 +156,14 @@ class UsersHandlers {
                 ctx.request.body.databases = ctx.request.body.databases ? [ ctx.request.body.databases ] : [];
             }
 
+            // ensure su is also admin
+            if (ctx.request.body.roles.includes('su') && !ctx.request.body.roles.includes('admin')) {
+                ctx.request.body.roles.concat(['admin'], ctx.request.body.roles); // put 'admin' at the front!
+            }
+
             await User.update(ctx.params.id, ctx.request.body);
+
+            // TODO: if roles/organisations changed for current user, need to reset ctx.state
 
             // return to list of users
             ctx.redirect('/users');
@@ -140,6 +200,19 @@ class UsersHandlers {
         }
     }
 
+}
+
+
+/**
+ * Determine list of available organisation databases by looking for environment variables starting
+ * with 'DB_'.
+ *
+ * @returns {string[]} List of defined organisation databases.
+ */
+function availableDatabases() {
+    const dbEnvVars = Object.keys(process.env).filter(envvar => envvar.slice(0,3)=='DB_');
+    const databases = dbEnvVars.map(db => db.slice(3).toLowerCase().replace('_', '-'));
+    return databases.filter(db => db!='users').sort();
 }
 
 

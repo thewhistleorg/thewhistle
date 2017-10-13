f335a5fd8db77f0e8897bc493c1aff0d4d404255
Chris Veness <chrisv@movable-type.co.uk>
2017-10-13 10:34:53 +0100

    Add title to dev notes

--- app-admin/routes-dev.js
+++ app-admin/routes-dev.js
@@ -7,6 +7,7 @@
 const router     = require('koa-router')(); // router middleware for koa
 const nodeinfo   = require('nodejs-info');  // node info
 const dateFormat = require('dateformat');   // Steven Levithan's dateFormat()
+const JsDom      = require('jsdom').JSDOM;  // JavaScript implementation of DOM and HTML standards
 
 const useragent  = require('../lib/user-agent.js');
 
@@ -25,7 +26,7 @@ router.get('/dev/user-agents', async function(ctx) {
 
 
 /* - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - -  */
-/*  Route to handle dev-notes pages                                                               */
+/* Route to handle dev-notes pages                                                                */
 /* - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - -  */
 
 const fs  = require('fs-extra');           // fs with extra functions & promise interface
@@ -37,13 +38,13 @@ md.use(mda);
 router.get('/dev/notes', async function getIndexPage(ctx) {
     const index = await fs.readFile('dev/index.md', 'utf8');
     const content = md.render(index);
-    await ctx.render('dev-notes', { content });
+    await ctx.render('dev-notes', { content, title: 'The Whistle Development Notes' });
 });
 
 router.get('/dev/notes/readme', async function getReadMePage(ctx) {
     const readme = await fs.readFile('README.md', 'utf8');
     const content = md.render(readme);
-    await ctx.render('dev-notes', { content });
+    await ctx.render('dev-notes', { content, title: 'The Whistle README' });
 });
 
 router.get('/dev/notes/:notes', async function getNotesPage(ctx) {
@@ -51,8 +52,10 @@ router.get('/dev/notes/:notes', async function getNotesPage(ctx) {
     try {
         const notesMarkdown = await fs.readFile(notesFile, 'utf8');
         const content = md.render(notesMarkdown);
-        await ctx.render('dev-notes', { content });
+        const document = new JsDom(content).window.document;
+        await ctx.render('dev-notes', { content, title: document.querySelector('h1').textContent });
     } catch (e) {
+        if (e.code != 'ENOENT') console.error(e);
         ctx.throw(404, 'Notes not found');
     }
 });

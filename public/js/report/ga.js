ce5c358aa2c3a9424c412d5885e6a4546015badb
Chris Veness <chrisv@movable-type.co.uk>
2017-10-20 10:36:54 +0100

    Add Google Analytics tracking to report app

---
+++ public/js/report/ga.js
@@ -0,0 +1,7 @@
+/**
+ * Google Analytics tracking code
+ */
+window.dataLayer = window.dataLayer || [];
+function gtag(){dataLayer.push(arguments);}
+gtag('js', new Date());
+gtag('config', 'UA-107905163-2');

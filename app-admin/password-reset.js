27733b4532085563fde7280155c6ecdd076d81d8
Chris Veness <chrisv@movable-type.co.uk>
2017-10-24 15:08:20 +0100

    Separate out e-mail send functionality to library

--- app-admin/password-reset.js
+++ app-admin/password-reset.js
@@ -4,27 +4,12 @@
 /* GET functions render template pages; POST functions process post requests then redirect.       */
 /* - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - -  */
 
-import nodemailer from 'nodemailer';   // sends e-mails from Node.js
-import handlebars from 'handlebars';   // logicless templating language
-import jsdom      from 'jsdom';        // DOM Document interface in Node!
-import htmlToText from 'html-to-text'; // converts html to beautiful text
-import fs         from 'fs-extra';     // fs with extra functions & promise interface
 import crypto     from 'crypto';       // nodejs.org/api/crypto.html
 import scrypt     from 'scrypt';       // scrypt library
 
 import User from '../models/user.js';
+import Mail from '../lib/mail.js';
 
-// nodemailer transporter
-const smtpConfig = {
-    host:   process.env.SMTP_HOST,
-    port:   process.env.SMTP_PORT,
-    secure: false,
-    auth:   {
-        user: process.env.SMTP_USER,
-        pass: process.env.SMTP_PASS,
-    },
-};
-const transporter = nodemailer.createTransport(smtpConfig);
 
 /*
  * Password reset sequence is:
@@ -36,6 +21,7 @@ const transporter = nodemailer.createTransport(smtpConfig);
  * - POST /password/reset/{token} with password & passwordConfirm; 302 ->
  * - GET  /password/reset/confirm
  */
+
 
 class PasswordResetHandlers {
 
@@ -71,32 +57,8 @@ class PasswordResetHandlers {
         // record reset request in db
         await User.update(user._id, { passwordResetRequest: token });
 
-        // get password reset template, completed with generated token
-        const templateHtml = await fs.readFile('app-admin/templates/password-reset.email.html', 'utf8');
-        const templateHbs = handlebars.compile(templateHtml);
-        const html = templateHbs({ firstname: user.firstname, host: ctx.host, token: token });
-
-        // get e-mail subject from <title> element
-        const document = new jsdom.JSDOM(html).window.document;
-        const subject = document.querySelector('title').textContent;
-
-        // prepare e-mail message
-        const message = {
-            from:    'noreply@thewhistle.org',
-            to:      email,
-            subject: subject,
-            html:    html,
-            text:    htmlToText.fromString(html),
-        };
-
-        // send out e-mail
-        try {
-            //const info = await transporter.verify(); TODO: ??
-            const info = await transporter.sendMail(message);
-            console.info('processRequest: info', info);
-        } catch (e) {
-            console.error('ERROR', e);
-        }
+        // send e-mail with generated token
+        Mail.send(email, 'password-reset.email', { firstname: user.firstname, host: ctx.host, token: token });
 
         ctx.set('X-Reset-Token', token); // for testing
 
@@ -176,7 +138,7 @@ async function userForResetToken(token) {
     if (token.split('-').length != 2) return null;
 
     // the token is a timestamp in base36 and a hash separated by a hyphen
-    const [ timestamp, hash ] = token.split('-');
+    const [ timestamp ] = token.split('-'); // (we don't need the hash here)
 
     // check token is not expired
     if (Date.now()/1000 - parseInt(timestamp, 36) > 60*60*24) return null;

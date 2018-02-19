5a5c127ebc980d816728fdc7ccea94c4c90b3f90
Chris Veness <chrisv@movable-type.co.uk>
2018-02-19 09:59:53 +0000

    Use 'import.meta' to determine current db/project

--- app-report/test-grn/sexual-assault/app.js
+++ app-report/test-grn/sexual-assault/app.js
@@ -8,11 +8,7 @@
 /* the routes directly from app-report.js this could be factored back there.                      */
 /* - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - -  */
 
-// TODO: before ES modules, directory name of the current module was available in __dirname; moving
-// forward, it is likely to be either 'import.meta' or 'import {url} from 'js:context'; see
-// - github.com/tc39/proposal-import-meta
-// - github.com/nodejs/node-eps/blob/master/002-es-modules.md#451-environment-variables
-const importMetaScriptElement = './app-report/test-grn/sexual-assault';
+import path from 'path'; // nodejs.org/api/path.html
 
 import Koa        from 'koa';            // koa framework
 import handlebars from 'koa-handlebars'; // handlebars templating
@@ -20,14 +16,16 @@ import handlebars from 'koa-handlebars'; // handlebars templating
 import HandlebarsHelpers from '../../../lib/handlebars-helpers.js';
 import ReportMiddleware  from '../../middleware.js';
 
+
 const app = new Koa(); // report app
 
 app.use(ReportMiddleware.mongoConnect()); // get db connection to ctx.params.database if not already available
 
 // handlebars templating (templates specific to this database/project)
+const handlebarsRoot = path.dirname(import.meta.url.replace(/.+\/app\//, '')); // get org/project from import.meta.url
 app.use(handlebars({
     extension:     [ 'html' ],
-    root:          importMetaScriptElement,
+    root:          handlebarsRoot,
     viewsDir:      './templates/pages',
     layoutsDir:    './templates',
     defaultLayout: 'layout',

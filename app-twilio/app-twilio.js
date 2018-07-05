f9b32f8f421195dd52efc420190c7f18a63dff87
dawson <dawson@treehill.com>
2018-07-05 10:03:30 +0100

    parse the top level question and output to console

--- app-twilio/app-twilio.js
+++ app-twilio/app-twilio.js
@@ -1,29 +1,46 @@
 const Koa = require('koa');
 const MessagingResponse = require('twilio').twiml.MessagingResponse;
 
-import Router from 'koa-router'; // router middleware for koa
+import Router     from 'koa-router';
+import jsonSchema from 'jsonschema';
+import $RefParser from 'json-schema-ref-parser';
+import serve      from 'koa-static';
+import dotProp    from 'dot-prop';
 
 const app = new Koa();
 const router = new Router();
 
+const spec = async () => {
+  return await $RefParser.dereference("http://twilio.thewhistle.local:3000/spec/grn/rape-is-a-crime.yaml");
+}
+
+app.use(serve('public', { maxage: 1000*60*60*24 }));
+
 router
-.get('/sms', ctx => {
-    console.log('!!!')
-    ctx.body = 'Yea!';
-    let smsCount = ctx.session.counter || 0;
-  
-    let message = 'Hello, thanks for the new message.';
-  
-    if(smsCount > 0) {
-      message = 'Hello, thanks for message number ' + (smsCount + 1);
+.get('/sms', async (ctx) => {
+
+  const ref = await spec();
+  ctx.response.body = ref;
+
+  // let smsCount = ctx.session.counter || 0;
+  // let message = 'Hello, thanks for the new message.';
+
+  // ctx.session.counter = smsCount + 1;
+
+  // const twiml = new MessagingResponse();
+  // twiml.message(message);
+
+  for (const p in ref.pages) {
+    for(const i in dotProp.get(ref, `pages.${p}`)) {
+      let t = dotProp.get(ref, `pages.${p}`)[i].text;
+      if (t != undefined & typeof t === "string") {
+        t.startsWith('#')==true & t.startsWith('##')==false && console.log(t);
+      }
     }
-  
-    ctx.session.counter = smsCount + 1;
-  
-    const twiml = new MessagingResponse();
-    twiml.message(message);
-  
-    ctx.body = twiml.toString();
+  }
+
+    // ctx.body = twiml.toString();
+    // ctx.body = spec();
 });
 
 app
